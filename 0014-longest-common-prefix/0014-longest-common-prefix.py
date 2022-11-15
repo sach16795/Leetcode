f77c0706be9b0246eb6401cbfbe35f6fff2ed40f
sach16795 <sach16795@gmail.com>
2022-11-15 19:08:44 +0800

    Time: 55 ms (73.13%), Space: 13.9 MB (50.06%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,7 +1,21 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # Approach 2
-        strs = sorted(strs, key = len)
+        # # Approach 2
+        # strs = sorted(strs, key = len)
+        # common = strs[0]
+        # for string in range(1, len(strs)):
+        #     if len(strs[string]) < len(common):
+        #         common = str(common[0:len(strs[string])])
+        #     for i in range(0, min(len(strs[string]), len(common))):
+        #         if strs[string][i] == common[i]:
+        #             continue
+        #         else:
+        #             common = str(common[0:i])
+        #             break
+        # return common
+        
+        
+        # Approach 1: Brute force
         common = strs[0]
         for string in range(1, len(strs)):
             if len(strs[string]) < len(common):
@@ -13,21 +27,4 @@ class Solution:
                     common = str(common[0:i])
                     break
         return common
-        
-        
-        # # Approach 1: Brute force
-        # common = strs[0]
-        # for string in range(1, len(strs)):
-        #     if len(strs[string]) < len(common):
-        #         common = str(common[0:len(strs[string])])
-        #     for i in range(0, min(len(strs[string]), len(common))):
-        #         print (i)
-        #         print(common)
-        #         print(strs[string])
-        #         if strs[string][i] == common[i]:
-        #             continue
-        #         else:
-        #             common = str(common[0:i])
-        #             break
-        # return common
         
