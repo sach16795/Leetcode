82dba83f00d76b14481064c403a6fe6b9458604a
sach16795 <sach16795@gmail.com>
2022-11-18 17:01:01 +0800

    Time: 347 ms (31.11%), Space: 16.6 MB (93.97%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # Approach 3:
+        vals = {}
         for num in nums:
-            if nums.count(num) == 1:
-                return num
-        # Approach 1: Naive iteration
+            if num in vals:
+                vals.pop(num)
+            else:
+                vals[num] = 1
+        return list(vals.keys())[0]
+        
+        # Approach 2: Naive iteration
+        # for num in nums:
+        #     if nums.count(num) == 1:
+        #         return num
+        
+        # Approach 1: Naive iteration with list
         # vals = []
         # for num in nums:
         #     if num in vals:
