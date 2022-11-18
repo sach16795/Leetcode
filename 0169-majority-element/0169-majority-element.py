e980f90a9ae3060d6aa077a24bc0f59f80a0793c
sach16795 <sach16795@gmail.com>
2022-11-18 17:47:43 +0800

    Time: 439 ms (28.07%), Space: 15.6 MB (35.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Approach 1:
-        vals = {}
-        for i in nums:
-            if i in vals:
-                vals[i] = vals[i] + 1
+        # Appraoch 2: Moores Voting
+        mc = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if mc != nums[i]:
+                count -= 1
+                if count == 0:
+                    mc = nums[i]
+                    count += 1
             else:
-                vals[i] = 1
-        for key,value in vals.items():
-            if value > (len(nums)/2):
-                return key
+                count += 1
+        return mc
+        
+        
+        # Approach 1:
+        # vals = {}
+        # for i in nums:
+        #     if i in vals:
+        #         vals[i] = vals[i] + 1
+        #     else:
+        #         vals[i] = 1
+        # for key,value in vals.items():
+        #     if value > (len(nums)/2):
+        #         return key
