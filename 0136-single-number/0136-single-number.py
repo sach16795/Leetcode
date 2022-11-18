98f7b25fc90713ed874ea76df0d8e19b48196e14
sach16795 <sach16795@gmail.com>
2022-11-18 17:16:41 +0800

    Time: 341 ms (34.57%), Space: 16.9 MB (21.04%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Approach 3:
-        vals = {}
-        for num in nums:
-            if num in vals:
-                vals.pop(num)
-            else:
-                vals[num] = 1
-        return list(vals.keys())[0]
+        # Approach 4: XOR 
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+        
+        # Approach 3: Dictionary
+        # vals = {}
+        # for num in nums:
+        #     if num in vals:
+        #         vals.pop(num)
+        #     else:
+        #         vals[num] = 1
+        # return list(vals.keys())[0]
         
         # Approach 2: Naive iteration
         # for num in nums:
