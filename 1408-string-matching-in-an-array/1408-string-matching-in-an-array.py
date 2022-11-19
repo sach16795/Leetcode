6c86760119e879ab91f36e8d1cf802d1d4468e38
sach16795 <sach16795@gmail.com>
2022-11-20 02:01:24 +0800

    Time: 84 ms (32.15%), Space: 13.9 MB (25.92%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = set()
+        if len(words) == 1:
+            return ans
+        for i in range(0, len(words)):
+            for j in range(i+1, len(words)):
+                if words[i] in words[j]:
+                    ans.add(words[i])
+                if words[j] in words[i]:
+                    ans.add(words[j])
+        return ans
+                    
+                    
