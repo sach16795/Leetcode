0688dff8de01f5237c4829f502580dc0d0b08d60
sach16795 <sach16795@gmail.com>
2022-11-23 20:12:28 +0800

    Time: 56 ms (53.18%), Space: 13.8 MB (75.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,3 +1,4 @@
+from collections import deque 
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,11 +7,34 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        # Approach 2: Iteration
+        def check (p,q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
             return True
-        if not p or not q:
-            return False
-        if p.val != q.val:
-            return False
-        else:
-            return self.isSameTree (p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        deq = deque([(p,q)])
+        
+        while deq:
+            p,q = deq.popleft()
+            if not check(p,q):
+                return False
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                    
+        return True
+        
+        # Approach 1: Recursion
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+        # if p.val != q.val:
+        #     return False
+        # else:
+        #     return self.isSameTree (p.left, q.left) and self.isSameTree(p.right, q.right)
