f60ae2f19c3ba81348c7f3fbb41e0baa36b55fdb
sach16795 <sach16795@gmail.com>
2023-11-05 11:18:05 +0800

    Time: 524 ms (56.52%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-#select employee_id, team_size from ((select employee_id, team_id from Employee) t1 left join (select team_id, count(team_id) as team_size from Employee group by team_id) t2 on t1.team_id = t2.team_id)
+select employee_id, team_size from ((select employee_id, team_id from Employee) t1 left join (select team_id, count(team_id) as team_size from Employee group by team_id) t2 on t1.team_id = t2.team_id)
 
-select employee_id, count(employee_id) over(partition by team_id) as team_size from Employee
+#select employee_id, count(employee_id) over(partition by team_id) as team_size from Employee
