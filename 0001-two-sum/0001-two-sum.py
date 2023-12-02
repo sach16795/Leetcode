85a1093260c3f3d3e3b63c77971f4e736eee9db9
sach16795 <sach16795@gmail.com>
2023-12-02 17:35:05 +0800

    Time: 58 ms (89.21%), Space: 17.7 MB (10.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-         # approach 1: Brute Force
-        # for i in range(0, len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if (nums[i] + nums[j] == target):
-        #             return [i,j]
-        #
-        # approach 2: Memory map
         # Map solution || Order N.
         seen = {}
         for idx, num in enumerate(nums):
