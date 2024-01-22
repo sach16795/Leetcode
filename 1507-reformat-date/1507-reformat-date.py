60f89e56764cfd647be3512f785175cb0e17f215
sach16795 <sach16795@gmail.com>
2024-01-22 23:22:25 +0800

    Time: 34 ms (73.96%), Space: 16.6 MB (61.19%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -2,7 +2,5 @@ class Solution:
     def reformatDate(self, date: str) -> str:
         month_map = {"Jan" :"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
         idx = date.split(" ")
-        date = idx[0][:-2]
-        month = month_map[idx[1]]
-        return idx[2] + "-" + str(month) + "-" + (str(0) +str(date) if len(date) ==1 else   str(date))
+        return idx[2] + "-" + month_map[idx[1]] + "-" + (str(0) +str(idx[0][:-2]) if len(idx[0][:-2]) ==1 else   str(idx[0][:-2]))
         
