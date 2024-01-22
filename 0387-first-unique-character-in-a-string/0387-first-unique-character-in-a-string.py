daf23ef03b496653c83bc0bf76f2eba549e2de00
sach16795 <sach16795@gmail.com>
2024-01-22 23:43:39 +0800

    Time: 4413 ms (14.50%), Space: 16.7 MB (59.02%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        seen = [0] * 26
-        ab = 'abcdefghijklmnopqrstuvwxyz'
         for i in s:
-            seen[ab.index(i)] += 1
-        for i in s:
-            if seen[ab.index(i)] == 1:
+            if s.count(i) == 1:
                 return s.index(i)
         return -1
-            
+        
         
