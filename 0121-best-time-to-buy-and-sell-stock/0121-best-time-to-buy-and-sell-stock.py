0c4fef97721f07ca15654c3a66397240fdf87e0c
sach16795 <sach16795@gmail.com>
2024-02-10 10:40:51 +0800

    Time: 708 ms (88.74%), Space: 27.5 MB (55.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Approach 1: Two pointer. Timed out.
-        # profit = 0
-        # for i in range(0,len(prices)):
-        #     for j in range (i+1, len(prices)):
-        #         if prices[j] - prices[i] > profit:
-        #             profit =  prices[j] - prices[i]
-        # return profit
-        # Approach 2
-        minseen = 100000
-        profit = 0 
-        for i in range(0, len(prices)):
-            if prices[i] < minseen:
-                minseen = prices[i]
-            if prices[i] - minseen > profit:
-                profit = prices[i] - minseen
-        return profit
-                
+        maxprof = 0
+        currmin = 1000000000
+        
+        for i in prices:
+            if i < currmin:
+                currmin = i
+            if maxprof < i - currmin:
+                maxprof = i - currmin
+        return maxprof
