1d32fbf9fea4911c8fe07dcf81be7621495a88a9
sach16795 <sach16795@gmail.com>
2024-02-11 17:00:03 +0800

    Time: 34 ms (67.55%), Space: 16.5 MB (89.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,10 @@
 class Solution:
-    vals = {1:1,
-           2:2}
     def climbStairs(self, n: int) -> int:
-        count = 0
-        if n == 1:
-            return self.vals[1]
-        if n == 2:
-            return self.vals[2]
-        elif n >= 3:
-            n_1 = self.climbStairs(n-1)
-            if n-1 not in self.vals:
-                self.vals[n-1] = n_1
-        return self.vals[n-1] + self.vals[n-2]
+        if n ==1:
+            return 1
+        if n ==2:
+            return 2
+        seen = {1:1,2:2}
+        for i in range(3,n+1):
+            seen[i] = seen[i-1] + seen[i-2]
+        return seen[n]
