f714d8c48a7c7d1370a1110f804aaaf73d60560f
sach16795 <sach16795@gmail.com>
2024-02-11 17:00:33 +0800

    Time: 29 ms (90.55%), Space: 16.7 MB (70.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n ==1:
-            return 1
-        if n ==2:
-            return 2
+        if n <= 2:
+            return n
         seen = {1:1,2:2}
         for i in range(3,n+1):
             seen[i] = seen[i-1] + seen[i-2]
