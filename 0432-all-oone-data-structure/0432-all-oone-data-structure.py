c28a0bb4114b0357bd2a7cb62c790f7fd21d9e12
sach16795 <sach16795@gmail.com>
2024-09-29 14:36:59 +0800

    Time: 148 ms (79.52%), Space: 32.5 MB (36.69%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,44 +1,152 @@
-class AllOne:
+class Node:
+    def __init__(self, freq):
+        self.freq = freq
+        self.prev = None
+        self.next = None
+        self.keys = set()
 
+
+class AllOne:
     def __init__(self):
-        self.seen = {}
-        
+        self.head = Node(0)  # Dummy head
+        self.tail = Node(0)  # Dummy tail
+        self.head.next = self.tail  # Link dummy head to dummy tail
+        self.tail.prev = self.head  # Link dummy tail to dummy head
+        self.map = {}  # Mapping from key to its node
 
     def inc(self, key: str) -> None:
-        if key in self.seen:
-            self.seen[key] += 1
-        else:
-            self.seen[key] = 1
-        
+        if key in self.map:
+            node = self.map[key]
+            freq = node.freq
+            node.keys.remove(key)  # Remove key from current node
+
+            nextNode = node.next
+            if nextNode == self.tail or nextNode.freq != freq + 1:
+                # Create a new node if next node does not exist or freq is not freq + 1
+                newNode = Node(freq + 1)
+                newNode.keys.add(key)
+                newNode.prev = node
+                newNode.next = nextNode
+                node.next = newNode
+                nextNode.prev = newNode
+                self.map[key] = newNode
+            else:
+                # Increment the existing next node
+                nextNode.keys.add(key)
+                self.map[key] = nextNode
+
+            # Remove the current node if it has no keys left
+            if not node.keys:
+                self.removeNode(node)
+        else:  # Key does not exist
+            firstNode = self.head.next
+            if firstNode == self.tail or firstNode.freq > 1:
+                # Create a new node
+                newNode = Node(1)
+                newNode.keys.add(key)
+                newNode.prev = self.head
+                newNode.next = firstNode
+                self.head.next = newNode
+                firstNode.prev = newNode
+                self.map[key] = newNode
+            else:
+                firstNode.keys.add(key)
+                self.map[key] = firstNode
 
     def dec(self, key: str) -> None:
-        if self.seen[key] > 1:
-            self.seen[key] -= 1
+        if key not in self.map:
+            return  # Key does not exist
+
+        node = self.map[key]
+        node.keys.remove(key)
+        freq = node.freq
+
+        if freq == 1:
+            # Remove the key from the map if freq is 1
+            del self.map[key]
         else:
-            self.seen.pop(key)
-        
+            prevNode = node.prev
+            if prevNode == self.head or prevNode.freq != freq - 1:
+                # Create a new node if the previous node does not exist or freq is not freq - 1
+                newNode = Node(freq - 1)
+                newNode.keys.add(key)
+                newNode.prev = prevNode
+                newNode.next = node
+                prevNode.next = newNode
+                node.prev = newNode
+                self.map[key] = newNode
+            else:
+                # Decrement the existing previous node
+                prevNode.keys.add(key)
+                self.map[key] = prevNode
+
+        # Remove the node if it has no keys left
+        if not node.keys:
+            self.removeNode(node)
 
     def getMaxKey(self) -> str:
-        if len(self.seen.values()) == 0 :
-            return ""
-        maxval = max(self.seen.values())
-        for key,val in self.seen.items():
-            if val == maxval:
-                return key
+        if self.tail.prev == self.head:
+            return ""  # No keys exist
+        return next(
+            iter(self.tail.prev.keys)
+        )  # Return one of the keys from the tail's previous node
 
     def getMinKey(self) -> str:
-        if len(self.seen.values()) == 0 :
-            return ""
-        minval = min(self.seen.values())
-        for key,val in self.seen.items():
-            if val == minval:
-                return key
+        if self.head.next == self.tail:
+            return ""  # No keys exist
+        return next(
+            iter(self.head.next.keys)
+        )  # Return one of the keys from the head's next node
+
+    def removeNode(self, node):
+        prevNode = node.prev
+        nextNode = node.next
+
+        prevNode.next = nextNode  # Link previous node to next node
+        nextNode.prev = prevNode  # Link next node to previous node
+        
+# Approach 1: Hashmap + iteration. Not O(1)
+# class AllOne:
+
+#     def __init__(self):
+#         self.seen = {}
+        
+
+#     def inc(self, key: str) -> None:
+#         if key in self.seen:
+#             self.seen[key] += 1
+#         else:
+#             self.seen[key] = 1
+        
+
+#     def dec(self, key: str) -> None:
+#         if self.seen[key] > 1:
+#             self.seen[key] -= 1
+#         else:
+#             self.seen.pop(key)
+        
+
+#     def getMaxKey(self) -> str:
+#         if len(self.seen.values()) == 0 :
+#             return ""
+#         maxval = max(self.seen.values())
+#         for key,val in self.seen.items():
+#             if val == maxval:
+#                 return key
+
+#     def getMinKey(self) -> str:
+#         if len(self.seen.values()) == 0 :
+#             return ""
+#         minval = min(self.seen.values())
+#         for key,val in self.seen.items():
+#             if val == minval:
+#                 return key
         
 
 
-# Your AllOne object will be instantiated and called as such:
-# obj = AllOne()
-# obj.inc(key)
-# obj.dec(key)
-# param_3 = obj.getMaxKey()
-# param_4 = obj.getMinKey()
+# # Your AllOne object will be instantiated and called as such:
+# # obj = AllOne()
+# # obj.inc(key)
+# # obj.dec(key)
+# # param_3 = obj.getMaxKey()
+# # param_4 = obj.getMinKey()
