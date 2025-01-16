ba710fce63ac47908672c079df2be1b4a0c5cff6
Saurabh Chaudhari <sach16795@gmail.com>
2025-01-16 22:03:02 +0800

    Time: 26 ms (97.85%), Space: 17.7 MB (31.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
+        if n <=1:
             return n
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        seen = {0:0,1:1}
+        for i in range(2,n+1):
+            seen[i] = seen[i-1] + seen[i-2]
+        return seen[n]
+        # Recursion
+        # if n <= 1:
+        #     return n
+        # else:
+        #     return self.fib(n-1) + self.fib(n-2)
