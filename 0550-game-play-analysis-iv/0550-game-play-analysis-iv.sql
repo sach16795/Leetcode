a159e3dd8a7bbdfd0b4405cfe4c4a02a686549e8
Saurabh Chaudhari <sach16795@gmail.com>
2026-01-03 11:28:12 +0800

    Time: 497 ms (87.11%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select round(count(distinct(player_id))/(select count(distinct(player_id)) from Activity),2) as fraction from 
-(select player_id, MIN(event_date) OVER (PARTITION BY player_id) as first_login, event_date from Activity)t0 where DATEDIFF(event_date, first_login) = 1;
+select round(count(distinct(player_id))/(select count(distinct(player_id)) from Activity ),2) as fraction from 
+(select player_id, FIRST_VALUE(event_date) over (partition by player_id order by event_date) as login, event_date from Activity) t0 where DATEDIFF(event_date,login) =1
